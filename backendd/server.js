23cd3471da35290d5b4312fc0d667f36a45714cb
Mahlet Mulugeta <161637299+mahletm2020@users.noreply.github.com>
2024-05-10 02:26:30 -0700

    server edited

--- backendd/server.js
+++ backendd/server.js
@@ -4,8 +4,8 @@ const customerRoutes = require('./routes/customer');
 const carRoutes = require('./routes/cars');
 const hotelRoutes = require('./routes/hotels');
 const tourGuideRoutes = require('./routes/tourguide');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const bookingRouter  = require('./routes/booking');
+const prisma  = require('./db');
 const path = require('path')
 const bodyParser = require('body-parser')
 
@@ -27,18 +27,37 @@ app.use('/customer', customerRoutes);
 app.use('/cars', carRoutes);
 app.use('/hotels', hotelRoutes);
 app.use('/tourguide', tourGuideRoutes);
+app.use('/booking', bookingRouter);
+
+
 
 
 app.get('/explore', async (_, res) => {
-const cars  = await prisma.car.findMany();
-const hotels  = await prisma.hotel.findMany();
-const tourguides  = await prisma.tourGuide.findMany();
-const sites  = await prisma.site.findMany();
-
-return res.render('explore', {
-    cars,hotels, tourguides, sites
+    const cars  = await prisma.car.findMany();
+    const hotels  = await prisma.hotel.findMany();
+    const tourguides  = await prisma.tourGuide.findMany();
+    const sites  = await prisma.site.findMany();
+   
+    return res.render('explore', {
+        cars,hotels, tourguides, sites
+    })
 })
- 
+
+app.get('/profile', async (_, res) => {
+    const bookings  = await prisma.booking.findMany({
+        include:{
+            car: true,
+            customer: true,
+            hotel: true,site: true, TourGuide: true
+        }
+    });
+
+    console.log(bookings)
+
+   
+    return res.render('profile', {
+        bookings
+    })
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
